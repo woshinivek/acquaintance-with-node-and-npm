c96153188d048457cdbb7457052c96a5697b9575
woshinivek <woshinivek@yahoo.com>
2022-05-26 17:08:14 -0400

    nothing specials

---
+++ index.js
@@ -0,0 +1,10 @@
+// const Joi = require("joi");
+
+// const passwordSchema = Joi.string().min(3).max(10).alphanum();
+
+// console.log(passwordSchema.validate("GGhh45"));
+// console.log(passwordSchema.validate("g"));
+
+const shortid = require("shortid");
+
+console.log(shortid.generate());
