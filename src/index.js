92fc85956bda96bdc19be2125fcf225fc4b96b1e
woshinivek <woshinivek@yahoo.com>
2022-05-27 18:54:11 -0400

    parcel working well

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import apiService from "./js/api-js";
+
+console.log(apiService);
+
+apiService.getId(45);
+
+apiService.getName("Tupang");
---
+++ src/js/api-js.js
@@ -0,0 +1,9 @@
+const getId = function (id) {
+  console.log(`Yor id: ${id}`);
+};
+
+const getName = function (name) {
+  console.log(`Your name: ${name}`);
+};
+
+export default { getId, getName };
